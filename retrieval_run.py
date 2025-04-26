648606b1a8c8bbb379d6eab9c69dfc310c1686e6
Nick Wogan <nicholaswogan@gmail.com>
2025-04-25 18:26:10 -0700

    resume

--- retrieval_run.py
+++ retrieval_run.py
@@ -87,7 +87,7 @@ if __name__ == '__main__':
             loglike,
             PRIOR,
             log_dir=out_dir, 
-            resume='overwrite'
+            resume='resume'
         )
 
         # Retrieval
